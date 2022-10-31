a8aa67bd9c32556ea5e79807ca762b9a77027687
Parsa Sadri <p_sadri_s@yahoo.com>
2022-10-31 15:08:43 +0200

    Add files via upload

---
+++ Orchestration and Microservices/database/models.py
@@ -0,0 +1,15 @@
+from .db import db
+
+
+class Album(db.Document):
+    name = db.StringField(required=True, unique=True)
+    description = db.StringField()
+
+class Photo(db.Document):
+    name = db.StringField(required=True)
+    tags = db.ListField(db.StringField())
+    location = db.StringField()
+    image_file = db.ImageField(required=True)
+    albums = db.ListField(db.ReferenceField(Album))
+    # complete the remaining code
+    
